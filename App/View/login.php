e9023b30ecde87745378f71ad2fd77734d077375
hamzaelboukri <hamzaelboukri01@gmail.com>
2025-02-04 13:55:07 +0100

    add login and sin up

---
+++ App/View/login.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+if(isset($_SESSION['user'])){
+    header("location:/YouDemy/public/index.php");
+}
+include_once $_SERVER['DOCUMENT_ROOT'].'/YouDemy/app/views/layout/header.php';
+// session_start();
+$error = isset($_SESSION['error']) ? $_SESSION['error'] : [];
+
+$name = $email = $password = "";
+
+
+
+extract(isset($_SESSION['old']) ? $_SESSION['old'] : []);
+// print_r($error);
+
+$_SESSION['old'] = null;
+$_SESSION['error'] = null;
+?>
+
+<section class="bg-black dark:bg-gray-900">
+    <div class="flex justify-center min-h-screen">
+        <div class="hidden bg-cover lg:block lg:w-2/5" style="background-image: url('https://images.unsplash.com/photo-1494621930069-4fd4b2e24a11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80')">
+        </div>
+
+        <div class="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
+            <div class="w-full">
+                <h1 class="text-2xl font-semibold tracking-wider text-white capitalize dark:text-white">
+                    Welcome Back!
+                </h1>
+
+                
+
+                <p class="mt-4 text-gray-300 dark:text-gray-400">
+                    Please enter your credentials to access your account.
+                </p>
+
+                <form class="grid grid-cols-1 gap-6 mt-8" action="/YouDemy/router/web.php" method="POST">
+                    <input type="hidden" name="url" value="logIn">
+                    <div>
+                        <label class="block mb-2 text-sm text-gray-300 dark:text-gray-200">Email Address</label>
+                        <input type="email" name="email" placeholder="you@example.com" class="block w-full px-5 py-3 mt-2 text-white placeholder-gray-400 bg-gray-800 border border-gray-600 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 focus:border-red-400 dark:focus:border-red-400 focus:ring-red-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+                        <h1 class='text-red-600'><?=isset($error['email'])?$error['email']:"" ?></h1>
+                    <div>
+                        <label class="block mb-2 text-sm text-gray-300 dark:text-gray-200">Password</label>
+                        <input type="password" name="password" placeholder="Enter your password" class="block w-full px-5 py-3 mt-2 text-white placeholder-gray-400 bg-gray-800 border border-gray-600 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 focus:border-red-400 dark:focus:border-red-400 focus:ring-red-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                    </div>
+
+                    <button
+                        name="submit"
+                        class="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-500 rounded-lg hover:bg-red-400 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-50">
+                        <span>Login</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 rtl:-scale-x-100" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                </form>
+                <div class="flex justify-between mt-4">
+                    <a href="/YouDemy/public/index.php" class="text-red-500 hover:underline">Go to Home</a>
+                    <p class="text-white capitalize">Don't have an account? <a href="./signUp.php" class="text-red-500 hover:underline">Sign up now</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+</body>
+</html>
---
+++ App/View/sing_up.php
@@ -0,0 +1,101 @@
+<?php
+    session_start();
+if(isset($_SESSION['user'])){
+    header("location:/YouDemy/public/index.php");
+}
+include_once $_SERVER['DOCUMENT_ROOT'].'/YouDemy/app/views/layout/header.php';
+$error = isset($_SESSION['error']) ? $_SESSION['error'] : [];
+
+$name = $email = $password = "";
+
+
+
+extract(isset($_SESSION['old']) ? $_SESSION['old'] : []);
+// print_r($_SESSION);
+
+$_SESSION['old'] = null;
+$_SESSION['error'] = null;
+?>
+<section class="bg-black dark:bg-gray-900">
+    <div class="flex justify-center min-h-screen">
+        <div class="hidden bg-cover lg:block lg:w-2/5" style="background-image: url('https://images.unsplash.com/photo-1494621930069-4fd4b2e24a11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80')">
+        </div>
+        <div class="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
+            <div class="w-full">
+                <h1 class="text-2xl font-semibold tracking-wider text-white capitalize dark:text-white">
+                    Get your free account now.
+                </h1>
+
+                <p class="mt-4 text-gray-300 dark:text-gray-400">
+                    Let’s get you all set up so you can verify your personal account and begin setting up your profile.
+                </p>
+
+                
+                <form class="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2" action="/YouDemy/router/web.php" method="POST">
+                    <input type="hidden" name="url" value="signUp">
+                    <div class="mt-6 col-span-2">
+                        <h1 class="text-gray-300 dark:text-gray-300">Select type of account</h1>
+    
+                        <div class="mt-3 md:flex md:items-center md:-mx-2">
+                            <label class="flex items-center">
+                                <input type="radio" name="accountType" value="student" class="hidden peer" id="student" />
+                                <span class="flex items-center justify-center w-full px-6 py-3 text-red-500 border border-red-500 rounded-lg cursor-pointer md:w-auto md:mx-2 dark:border-red-400 dark:text-red-400 hover:bg-red-500 hover:text-white transition duration-200 peer-checked:bg-red-500 peer-checked:text-white">
+                                    <span class="mx-2">Student</span>
+                                </span>
+                            </label>
+                            
+                            <label class="flex items-center mt-4 md:mt-0">
+                                <input type="radio" name="accountType" value="teacher" class="hidden peer" id="teacher" />
+                                <span class="flex items-center justify-center w-full px-6 py-3 text-red-500 border border-red-500 rounded-lg cursor-pointer md:w-auto md:mx-2 dark:border-red-400 dark:text-red-400 hover:bg-red-500 hover:text-white transition duration-200 peer-checked:bg-red-500 peer-checked:text-white">
+                                    <span class="mx-2">Teacher</span>
+                                </span>
+                            </label>
+                        </div>
+                    <h1 class="text-red-600"><?php echo $error['role'] ?? ''; ?></h1>
+
+                        </div>
+                    <div>
+                        <label class="block mb-2 text-sm text-gray-300 dark:text-gray-200">Full Name</label>
+                        <input type="text" name="name" value="<?=isset($name)?$name:''?>" placeholder="John" class="block w-full px-5 py-3 mt-2 text-white placeholder-gray-400 bg-gray-800 border border-gray-600 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 focus:border-red-400 dark:focus:border-red-400 focus:ring-red-400 focus:outline-none focus:ring focus:ring-opacity-40"/>
+                    <h1 class="text-red-600"><?php echo $error['name'] ?? ''; ?></h1>
+
+                    </div>
+
+                    <div>
+                        <label class="block mb-2 text-sm text-gray-300 dark:text-gray-200">Email Address</label>
+                        <input type="text" name="email" value="<?=isset($email)?$email:''?>" placeholder="johnsnow@example.com" class="block w-full px-5 py-3 mt-2 text-white placeholder-gray-400 bg-gray-800 border border-gray-600 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 focus:border-red-400 dark:focus:border-red-400 focus:ring-red-400 focus:outline-none focus:ring focus:ring-opacity-40"/>
+                        <h1 class="text-red-600"><?php echo $error['email'] ?? ''; ?></h1>
+                    </div>
+
+                    <div>
+                        <label class="block mb-2 text-sm text-gray-300 dark:text-gray-200">Password</label>
+                        <input type="password" name="password" placeholder="Enter your password" class="block w-full px-5 py-3 mt-2 text-white placeholder-gray-400 bg-gray-800 border border-gray-600 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 focus:border-red-400 dark:focus:border-red-400 focus:ring-red-400 focus:outline-none focus:ring focus:ring-opacity-40"/>
+                    <h1 class="text-red-600"><?php echo $error['password'] ?? ''; ?></h1>
+
+                    </div>
+
+                    <div>
+                        <label class="block mb-2 text-sm text-gray-300 dark:text-gray-200">Confirm Password</label>
+                        <input type="password" name="pswComfirmation" placeholder="Re-enter your password" class="block w-full px-5 py-3 mt-2 text-white placeholder-gray-400 bg-gray-800 border border-gray-600 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 focus:border-red-400 dark:focus:border-red-400 focus:ring-red-400 focus:outline-none focus:ring focus:ring-opacity-40"/>
+                    </div>
+                    
+                    <div class="flex items-center w-full col-span-2 gap-2">
+                        <button name="submit"
+                            class="w-1/2 flex items-center justify-between px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-500 rounded-lg hover:bg-red-400 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-50">
+                            <span>Sign Up</span>
+                        </button>
+                    </div>
+                    
+                    <div class="flex justify-between col-span-2 mt-4">
+                        <p class="text-white capitalize">I have an account? <a href="./logIn.php" class="text-red-500 hover:underline">Login</a></p>
+                        <a href="/YouDemy/public/index.php" class="items-center justify-between px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-500 rounded-lg hover:bg-red-400 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-50">
+                            Back to Home</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+</body>
+</html>
