29f6eb119fbef2bacc05e68773f465b885a71ab2
hamzaelboukri <hamzaelboukri01@gmail.com>
2025-02-04 13:48:37 +0100

    add namespase

---
+++ App/config/Database.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\App;
+use PDO; 
+
+class Database {
+    private static $host = "localhost";
+    private static $db = "carMVC";
+    private static $username = "root";
+    private static $password = "";
+    private static $driver = "mysql";    
+    private static $conn = null;        
+
+    public static function getConnection() {  
+        if (self::$conn) {            
+            return self::$conn;
+        }
+        
+        try {
+            self::$conn = new PDO(
+                self::$driver . ":host=" . self::$host . ";dbname=" . self::$db,
+                self::$username,
+                self::$password
+            );
+            self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return self::$conn;
+        } catch (PDOException $e) {
+            throw new Exception("Connection failed: " . $e->getMessage());
+        }
+    }
+}
+?>
+
+
+
+
+
+
+
